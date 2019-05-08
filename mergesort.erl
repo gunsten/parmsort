8bf546e89486b8faf236f5617d81249847813591
gunsten <gunnar.stenlund@gmail.com>
2019-05-08 17:50:27 +0200

    Implements eqc tests for merge, based on sort

--- mergesort.erl
+++ mergesort.erl
@@ -1,4 +1,6 @@
 -module(mergesort).
+-include_lib("eqc/include/eqc.hrl").
+-import(lists, [sort/1]).
 -compile(export_all).
 
 mergesort() -> ok.
@@ -7,3 +9,7 @@ merge([], Ys) -> Ys;
 merge(Xs, []) -> Xs;
 merge([X|Xs], [Y|Ys]) when X =< Y -> [X | merge(Xs, [Y|Ys])];
 merge(Xs, [Y|Ys]) -> [Y | merge(Xs, Ys)].
+
+prop_merge() -> ?FORALL(Xs, list(nat()),
+                    ?FORALL(Ys, list(nat()),
+                        sort(Xs ++ Ys) == sort(merge(Xs, Ys)) )).
