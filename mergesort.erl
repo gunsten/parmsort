6265cfd0419623b561d82f4835f23a3a1fc7d237
gunsten <gunnar.stenlund@gmail.com>
2019-05-08 17:37:44 +0200

    Implements merge

--- mergesort.erl
+++ mergesort.erl
@@ -1,3 +1,9 @@
 -module(mergesort).
+-compile(export_all).
 
 mergesort() -> ok.
+
+merge([], Ys) -> Ys;
+merge(Xs, []) -> Xs;
+merge([X|Xs], [Y|Ys]) when X =< Y -> [X | merge(Xs, [Y|Ys])];
+merge(Xs, [Y|Ys]) -> [Y | merge(Xs, Ys)].
